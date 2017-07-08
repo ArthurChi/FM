4cba9e9736f5bbe5637ad1538e3ebde4961a665d
cjfire <cjfire@sina.cn>
2017-07-08 13:09:24 +0800

    项目建立

---
+++ FMAudioPlayer/FMAudioPlayer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FMAudioPlayer
+//
+//  Created by cjfire on 2017/7/8.
+//  Copyright © 2017年 cjfire. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
